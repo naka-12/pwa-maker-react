47ce3b353db971aa0c6df6316b92b135c88940a6
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-06-06 14:34:52 +0900

    introduce react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import Editor from "@monaco-editor/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <Editor
-      height="100vh"
-      language="javascript"
-      value="console.log('Hello, World!');"
-    />
+    <Row>
+      <Col>
+        <h2 className="mx-3">オリジナルアプリを作ろう！</h2>
+      </Col>
+      <Col>
+        <Editor height="30vh" language="html" value="<p>Hello, World!</p>" />
+        <Editor height="30vh" language="css" value="p { color: red; }" />
+        <Editor
+          height="30vh"
+          language="javascript"
+          value="console.log('Hello, World!');"
+        />
+      </Col>
+      <Col>
+        <h3 className="title">プレビュー</h3>
+      </Col>
+    </Row>
   );
 }
 
